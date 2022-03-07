9ceb6aaced185fc7b591ac25af126e0728c68ae8
Stephan Klinger <staeff@arcor.de>
2022-03-07 22:07:00 +0100

    Use json instead of xml as input for process_images

--- process_images.py
+++ process_images.py
@@ -1,18 +1,16 @@
 from pathlib import Path
 from PIL import Image
-from lxml import etree
+import json
 import os
-
-NS = "{http://www.loc.gov/standards/alto/ns-v2#}"
 
 
 def crop_image(img_obj, features, outpath):
     """Extract the image by given coordinates
 
-        feature is a dict.
-        feature["coords"] is a tuple with 4 numerical
-        values defining the position of the element on the
-        page.
+    feature is a dict.
+    feature["coords"] is a tuple with 4 numerical
+    values defining the position of the element on the
+    page.
     """
     file_id = features["file_id"]
     block_id = features["block_id"]
@@ -22,73 +20,53 @@ def crop_image(img_obj, features, outpath):
 
 def extract_id(block_id_string):
     """Gets something block id string like Page1_Block1
-       and returns the remaining number when Page1_Block
-       is removed.
+    and returns the remaining number when Page1_Block
+    is removed.
     """
     return block_id_string.replace("Page1_Block", "")
 
 
-def get_coordinates(item_attrs):
+def get_coordinates(fields):
     """Get coordinates of a block"""
-    x0 = int(item_attrs["HPOS"])
-    y0 = int(item_attrs["VPOS"])
-    x1 = int(item_attrs["WIDTH"]) + x0
-    y1 = int(item_attrs["HEIGHT"]) + y0
+    x0 = int(fields["x"])
+    y0 = int(fields["y"])
+    x1 = int(fields["width"]) + x0
+    y1 = int(fields["height"]) + y0
     return (x0, y0, x1, y1)
 
-def get_features(block):
-    """Get attributes from block
-        that are needed for further processing
-    """
-    item_attrs = block.attrib
-    # Assign nodes attributes dict to a var
-    features = {
-        "coords": get_coordinates(item_attrs),
-        "block_id": extract_id(item_attrs["ID"]),
-        "file_id": file_id,
-    }
-    return features
 
 def create_path(file_id):
     """Create a folder for every Page
 
-        e.g. /vw-1891-01-04-3-004/
-        To organize the images of ads, that each is
-        split into.
+    e.g. /vw-1891-01-04-3-004/
+    To organize the images of ads, that each is
+    split into.
     """
     cwd = Path.cwd()
     path = f"images/{file_id}/"
     dirname = cwd / path
     if not dirname.exists():
+        print(f"Creating {dirname}")
         os.makedirs(dirname, exist_ok=True)
     return dirname
 
 
-def get_xmlfile_paths():
-    """Return pathlib Paths of xml files processed."""
-    cwd = Path.cwd()
-    xml_files = sorted(list(cwd.glob("datafolder/xml/*.xml")))
-    return xml_files
-
-
 if __name__ == "__main__":
+    with open("advertisments.json", "r") as infile:
+        data = json.load(infile)
 
-    xml_files = get_xmlfile_paths()
+    for entry in data:
+        fields = entry["fields"]
+        file_id = fields["file_id"]
+        features = {
+            "coords": get_coordinates(fields),
+            "block_id": fields["block_id"],
+            "file_id": file_id,
+        }
 
-    for i, xml_file in enumerate(xml_files):
-        file_id = xml_file.stem
         outpath = create_path(file_id)
 
         # Turn original image into a PIL obj
         # XXX Do we have to do this each iteration?
         img_obj = Image.open(f"datafolder/images/{file_id}.jpg")
-
-        # parse XML file into etree
-        tree = etree.parse(str(xml_file))
-
-        # Extract all textblocks elements
-        textblocks = tree.findall(f".//{NS}TextBlock")
-
-        for block in textblocks:
-            features = get_features(block)
-            crop_image(img_obj, features, outpath)
+        crop_image(img_obj, features, outpath)
