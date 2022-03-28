9bb5545e364b3ff2b6eb8100236bb096eb46c795
Stephan Klinger <staeff@arcor.de>
2022-03-28 21:09:22 +0200

    Parallelize image processing
    unparallel
    
    $ time python process_images.py
    python process_images.py  80.83s user 3.64s system 94% cpu 1:29.43 total
    
    with multiprocessing.Pool.map
    
    $ time python process_images.py
    python process_images.py  128.07s user 5.10s system 341% cpu 38.987 total

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 import os
+import sys
 
 def create_path(path_name):
     """
@@ -19,3 +20,7 @@ def get_s3_bucket(bucket_name):
     """Connect to a s3 bucket for further operations"""
     s3 = boto3.resource('s3')
     return s3.Bucket(bucket_name)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.argv
