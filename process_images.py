540c76865a48c30a47438540180ecd38c46a5b90
Stephan Klinger <staeff@arcor.de>
2022-03-29 23:21:29 +0200

    Upload crop ad images to s3

--- process_images.py
+++ process_images.py
@@ -1,10 +1,16 @@
 from utils import create_path
+from utils import get_s3_bucket
 from PIL import Image
+import io
 import multiprocessing as mp
 import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+AWS_AD_IMAGES_BUCKET = os.getenv('AWS_AD_IMAGES_BUCKET')
 
-def crop_image(img_obj, features):
+def crop_image(img_obj, features, s3bucket):
     """Extract the image by given coordinates
 
     feature is a dict.
@@ -14,8 +20,16 @@ def crop_image(img_obj, features):
     """
     file_id = features["file_id"]
     block_id = features["block_id"]
+    image_name = f"{file_id}-{block_id}.jpg"
+    image_path = f"images/ad_images/{image_name}"
     cropped = img_obj.crop(features["coords"])
-    cropped.save(f"images/ad_images/{file_id}-{block_id}.jpg")
+
+    cropped.save(image_path, img_obj.format)
+    in_mem_img = io.BytesIO()
+    cropped.save(in_mem_img, img_obj.format)
+    in_mem_img.seek(0)
+    s3msg = s3bucket.put_object(Key=image_name, Body=in_mem_img)
+    print(s3msg)
 
 
 def extract_id(block_id_string):
@@ -44,9 +58,12 @@ def process_entry(entry):
         "file_id": file_id,
     }
 
+    #
+    s3bucket = get_s3_bucket(AWS_AD_IMAGES_BUCKET)
+
     # Turn original image into a PIL obj
     img_obj = Image.open(f"datafolder/images/{file_id}.jpg")
-    crop_image(img_obj, features)
+    crop_image(img_obj, features, s3bucket)
 
 if __name__ == "__main__":
 
